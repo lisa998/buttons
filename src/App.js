ca4e5331e5034e18881d7db532f0a92761af20ac
lisa <xj3zjfan@gmail.com>
2022-02-17 17:24:37 +0800

    buttons

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import * as React from "react";
+import { useState } from "react";
+import Button from "@mui/material/Button";
+import ButtonGroup from "@mui/material/ButtonGroup";
+import Box from "@mui/material/Box";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [clickable, setClickable] = useState(true);
+  const plus = () => {
+    setCount((state) => state + 1);
+  };
+  const clear = () => {
+    setCount(0);
+  };
+  const disable = () => {
+    setClickable((state) => !state);
+  };
+
+  const buttons = [
+    <Button key="one" onClick={plus} disabled={!clickable}>
+      CLICK:{count}
+    </Button>,
+    <Button key="two" onClick={clear}>
+      CLEAR
+    </Button>,
+    <Button key="three" onClick={disable}>
+      DISABLE
+    </Button>,
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <Box
+        sx={{
+          display: "flex",
+          "& > *": {
+            m: 1,
+          },
+        }}
+      >
+        <ButtonGroup
+          orientation="vertical"
+          aria-label="vertical outlined button group"
         >
-          Learn React
-        </a>
-      </header>
+          {buttons}
+        </ButtonGroup>
+      </Box>
     </div>
   );
 }
